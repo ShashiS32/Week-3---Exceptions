8fc0c4d24df9b2d2b991515a5f22aad8a6e335ab
Shashwat Singh <shashwats17@gmail.com>
2024-06-21 18:36:18 -0400

    skib

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 try:
     x = int(input("What is x? "))
-    print (f"x is {x}")
 except ValueError:
     print("X needs to be an int, you didnt write an int")
     
+else:
+
+    print (f"x is {x}")
+ 
